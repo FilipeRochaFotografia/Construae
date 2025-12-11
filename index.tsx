41ccacaad09704cd48e06ba1cb1370ecad4fb8a5
FilipeRochaFotografia <filiperochafotografia@gmail.com>
2025-12-11 19:54:23 -0300

    Index

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; 
 
 // Renderização padrão React 18/19
 ReactDOM.createRoot(document.getElementById('root')!).render(
